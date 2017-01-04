7d3c9b01214a234ee928406912010de3d9e9f623
Junseong Park <js@kweiza.com>
2017-01-04 13:40:46 +0900

    added example for style

--- src/Sandbox.js
+++ src/Sandbox.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+//import style file
+import '../styles/Sandbox.scss';
+
 export default class Sandbox extends React.Component {
     render() {
         return (
-            <div>
+            <div className='sandbox'>
                 test!!
             </div>
         );
